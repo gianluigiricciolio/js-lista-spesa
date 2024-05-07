32ff4a9434b02ebf533c7c4dd503fda1559c7c44
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-07 09:47:08 +0200

    [UPD] comments

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-console.log('sono caricato');
+
+
 
 const listaSpesa = [
     "Latte",
@@ -11,16 +12,22 @@ const listaSpesa = [
     "Insalata",
 ]
 
+// DICHIARO LA VARIABILE CONTATORE DA USARE NEL CICLO WHILE
 let count=0;
 
+
+// CREO UNA VARIABILE A CUI ASSEGNO UN OGGETTO DAL DOM
 let ul = document.querySelector('.lista');
-console.log(ul);
 
 while (count<listaSpesa.length){
+    // CREDO UNA VARIABILE TEMPORANEA ATTA A CONTENERE UN LI
     let li = document.createElement('li');
+    // COPIO L'ELEMENTO IN POSIZIONE I NELL ELEMENTO LI
     li.innerText = listaSpesa[count];
     console.log(li.innerHTML);
+    // INSERISCO L' ELEMENTO LI NELLA MIA LISTA
     ul.append(li);
     console.log(ul.innerHTML);
+    // INCREMENTO LA VARIABILE CONTATORE PER SCORRERE LA LISTA
     count++;
 }
